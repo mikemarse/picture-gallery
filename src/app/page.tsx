fb16ac0a6a5885cb10c39ce3e775cccce124231e
mikemarse <134131019+mikemarse@users.noreply.github.com>
2025-12-07 18:01:27 -0600

    Link database content to UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,32 +3,22 @@ import { db } from "~/server/db";
 
 export const dynamic = "force-dynamic";
 
-const mockUrl = [
-	"https://utfs.io/f/4033f3b2-0fc9-4a6e-95be-37009f50e078-7qajqq.png",
-	"https://utfs.io/f/4033f3b2-0fc9-4a6e-95be-37009f50e078-7qajqq.png",
-	"https://utfs.io/f/4033f3b2-0fc9-4a6e-95be-37009f50e078-7qajqq.png",
-	"https://utfs.io/f/4033f3b2-0fc9-4a6e-95be-37009f50e078-7qajqq.png",
-];
-
-const mockPost = mockUrl.map((url, index) => ({
-	id: index + 1,
-	url,
-}));
-
 export default async function HomePage() {
 
-	const posts = await db.query.posts.findMany();
+	const images = await db.query.images.findMany({
+		orderBy: (models, { desc }) => desc(models.id),
+	});
 
   return (
 		<main className="p-4">
 			<div className="flex flex-wrap gap-4">
-				{posts.map((post) => (<div key={post.id}>{post.name}</div>))}
-				{[...mockPost, ...mockPost, ...mockPost].map((image, index) => (
-					<div key={image.id + '-' + index} className="w-48">
-						<img src={image.url}/>
+				{[...images, ...images, ...images].map((post) => (<div className="w-48 flex flex-col" key={post.id}>{
+					<div className="flex flex-col gap-1">
+						<img src={post.url}/>
+						<div>{post.name}</div>
+
 					</div>
-					))
-				}
+				}</div>))}
 			</div>
 		</main>
 	);
--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -11,11 +11,12 @@ import { index, pgTableCreator } from "drizzle-orm/pg-core";
  */
 export const createTable = pgTableCreator((name) => `picture-gallery_${name}`);
 
-export const posts = createTable(
-  "post",
+export const images = createTable(
+  "images",
   (d) => ({
     id: d.integer().primaryKey().generatedByDefaultAsIdentity(),
-    name: d.varchar({ length: 256 }),
+    name: d.varchar({ length: 256 }).notNull(),
+		url: d.varchar({ length: 1025 }).notNull(),
     createdAt: d
       .timestamp({ withTimezone: true })
       .$defaultFn(() => /* @__PURE__ */ new Date())
