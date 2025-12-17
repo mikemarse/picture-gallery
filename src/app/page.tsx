8d994e14b0c8ad6d85f7f513c9559cfddeebd6d9
mikemarse <134131019+mikemarse@users.noreply.github.com>
2025-12-16 23:38:23 -0600

    Added auth on main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,40 @@
+import { SignedIn, SignedOut } from "@clerk/nextjs";
 import Link from "next/link";
 import { db } from "~/server/db";
 
 export const dynamic = "force-dynamic";
 
-export default async function HomePage() {
+async function Images() {
 
 	const images = await db.query.images.findMany({
-		orderBy: (models, { desc }) => desc(models.id),
+		orderBy: (models, { asc }) => asc(models.id),
 	});
+
+	return (
+		<div className="flex flex-wrap gap-4">
+			{images.map((post) => (<div className="w-48 flex flex-col" key={post.id}>{
+				<div className="flex flex-col gap-1">
+					<img src={post.url}/>
+					<div>{post.name}</div>
+
+				</div>
+			}</div>))}
+		</div>
+
+		);
+}
+
+export default async function HomePage() {
+
 
   return (
 		<main className="p-4">
-			<div className="flex flex-wrap gap-4">
-				{images.map((post) => (<div className="w-48 flex flex-col" key={post.id}>{
-					<div className="flex flex-col gap-1">
-						<img src={post.url}/>
-						<div>{post.name}</div>
-
-					</div>
-				}</div>))}
-			</div>
+			<SignedOut>
+				<div className="h-full w-full text-2xl text-center justify-center">Please sign in above</div>
+			</SignedOut>
+			<SignedIn>
+				<Images />
+			</SignedIn>
 		</main>
 	);
 }
