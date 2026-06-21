f9da456b42d3a48e19c8212666261545154689c8
mikemarse <134131019+mikemarse@users.noreply.github.com>
2026-06-21 14:36:08 -0500

    Making a dedicated query file

---
+++ src/server/queries.ts
@@ -0,0 +1,15 @@
+import "server-only";
+import { db } from "./db";
+import { auth } from "@clerk/nextjs/server";
+
+export async function getMyImages() {
+  const { userId } = await auth();
+
+  if (!userId) throw new Error("Unauthorized");
+
+  const images = await db.query.images.findMany({
+    where: (model, { eq }) => eq(model.userId, userId),
+    orderBy: (model, { desc }) => desc(model.id),
+  });
+  return images;
+}
